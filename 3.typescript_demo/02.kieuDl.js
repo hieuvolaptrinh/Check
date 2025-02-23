6070ed495860782abef0b61c3460856df45516fd
hieuvolaptrinh <vndhieuak@gmail.com>
2025-02-23 20:39:19 +0700

    data   type

---
+++ 3.typescript_demo/02.kieuDl.js
@@ -0,0 +1,4 @@
+var myNumber = 10000;
+var myBoolean = "võ nguyễn đại hiêu";
+var myName = true;
+console.log(myNumber, myBoolean, myName);
---
+++ 3.typescript_demo/02.kieuDl.ts
@@ -0,0 +1,6 @@
+{
+  let myNumber: number = 10000;
+  let myBoolean: string = "võ nguyễn đại hiêu";
+  let myName: boolean = true;
+  console.log(myNumber, myBoolean, myName);
+}
